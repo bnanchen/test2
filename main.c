a096f2a279b96124f9d7c85060d842599b51a553
bnanchen <bastiannanchen@Macintosh-3.local>
2016-02-24 11:12:35 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Serie01
+//
+//  Created by Bastian Nanchen on 24/02/16.
+//  Copyright © 2016 Bastian Nanchen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Quel âge avez-vous? \n");
+    int age = 0;
+    scanf("%d", &age);
+    int annee = 2016 - age;
+    printf("L'année de naissance est %d \n", annee);
+    return 0;
+}
